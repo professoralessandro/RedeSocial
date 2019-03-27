7e3c567994efa11692f66862ae364a9f830eaa90
professoralessandro <professor_alessandro@hotmail.com>
2019-03-27 17:22:04 -0300

    atual

--- Classes/Controller/DALUsuario.php
+++ Classes/Controller/DALUsuario.php
@@ -93,15 +93,15 @@ class DALUsuario
     }
 	
 	//FUNÇÃO  PRIMEIRO NOME
-    public function primeiroNome($str)
-    {
-	$pos_espaco = strpos($str, ' ');// perceba que há um espaço aqui
-	$primeiro_nome = substr($str, 0, $pos_espaco);
-	$resto_nome = substr($str, $pos_espaco, strlen($str));
+	public function primeiroNome($str)
+	{
+		$pos_espaco = strpos($str, ' ');// perceba que há um espaço aqui
+		$primeiro_nome = substr($str, 0, $pos_espaco);
+		$resto_nome = substr($str, $pos_espaco, strlen($str));
 
-	return $primeiro_nome;
-	//return array('primeiro_nome'=> $primeiro_nome, 'resto_nome' => $resto_nome);
-    }
+		return $primeiro_nome;
+		//return array('primeiro_nome'=> $primeiro_nome, 'resto_nome' => $resto_nome);
+	}
 	
 	//FUNÇÃO TRATAR DDD
     function buscarCaracteres($ddd)
