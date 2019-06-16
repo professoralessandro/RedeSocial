5c876f9d1915b95e1074c299239133ee4097ebd8
professoralessandro <36211282+professoralessandro@users.noreply.github.com>
2019-06-16 12:03:33 -0300

    Mudança de referencia do bootstrap da internet

--- Classes/View/Usuario/buscarMedicosPalavra.php
+++ Classes/View/Usuario/buscarMedicosPalavra.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['usuarioNome'])) {
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>Bootstrap Agency Page Template</title>
             <!-- Bootstrap -->
-            <link href="../../../../css/bootstrap-4.0.0.css" rel="stylesheet">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         </head>
         <body>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -187,10 +187,9 @@ if (isset($_SESSION['usuarioNome'])) {
                     </footer>
                     <!-- FIM FOOTER -->
                     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-                    <script src="../../../../js/jquery-3.2.1.min.js"></script> 
-                    <!-- Include all compiled plugins (below), or include individual files as needed --> 
-                    <script src="../../../../js/popper.min.js"></script> 
-                    <script src="../../../../js/bootstrap-4.0.0.js"></script>
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
                     </body>
                     </html>
                     <?php
